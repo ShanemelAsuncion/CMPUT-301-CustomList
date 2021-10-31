1644e7c4ede25a87ac34f1f33d46ce0f812ac1c5
Shanemel Asuncion <shanemelasuncion@gmail.com>
2021-10-31 02:26:52 -0600

    deleteCity must pass

--- app/src/main/java/com/example/simpleparadox/listycity/CustomList.java
+++ app/src/main/java/com/example/simpleparadox/listycity/CustomList.java
@@ -52,8 +52,7 @@ public class CustomList extends ArrayAdapter<City> {
 //    @return
 
     public int getCount() {
-//        return cities.size();
-        return -1;
+        return cities.size();
     }
 
 //    this function will add a city object into the list
