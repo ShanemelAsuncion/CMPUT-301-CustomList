d3645c7752a8ea8442344e0a2e42474bc1d1f144
Shanemel Asuncion <shanemelasuncion@gmail.com>
2021-10-31 04:14:01 -0600

    countCitiesTest pass

--- app/src/main/java/com/example/simpleparadox/listycity/CustomList.java
+++ app/src/main/java/com/example/simpleparadox/listycity/CustomList.java
@@ -81,8 +81,7 @@ public class CustomList extends ArrayAdapter<City> {
 
 
     public int countCities(){
-        //return cities.size();
-        return -1;
+        return cities.size();
     }
 
 
